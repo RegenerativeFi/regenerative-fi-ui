f4a3fce658e0e6986589f560a1075ec34c42ab2b
Another <joseluismanco37@gmail.com>
2024-01-03 17:29:29 -0500

    chore. reestructure data fetching

---
+++ src/constants/geckoterminal.ts
@@ -0,0 +1,18 @@
+export const MockedTestnetTokens = {
+  celo: {
+    '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE':
+      '0x471EcE3750Da237f93B8E339c536989b8978a438',
+    '0x524d97a67f50f4a062c28c74f60703aec9028a94':
+      '0x66803fb87abd4aac3cbb3fad7c3aa01f6f3fb207',
+    '0x1d4C35C3F4a91103bA323fE2f4c3f6eBeF531c11':
+      '0x66803fb87abd4aac3cbb3fad7c3aa01f6f3fb207',
+    '0x317e1d46ceE079683C82fFfC9b524F55c29B7b91':
+      '0x66803fb87abd4aac3cbb3fad7c3aa01f6f3fb207',
+    '0x4a4AC35B50DAf081C22856ebB26561F3a22598C6':
+      '0x765de816845861e75a25fca122bb6898b8b1282a',
+  },
+};
+
+export const geckoTerminalNetworks = {
+  44787: 'celo',
+};
