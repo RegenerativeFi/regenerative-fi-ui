be2f6f38c6cead7c533bd4a871f8ba2e99a28f80
anotherdev <joseluismanco37@gmail.com>
2025-03-17 23:17:50 -0500

    chore: remove add incentives from staked pools

--- src/components/contextual/pages/dashboard/DashboardTable/StakedLiquidityTable.vue
+++ src/components/contextual/pages/dashboard/DashboardTable/StakedLiquidityTable.vue
@@ -36,11 +36,7 @@ const {
   refetchAllUserPools,
   isLoading: isLoadingPools,
 } = useUserPools();
-const defaultPoolActions = [
-  PoolAction.Unstake,
-  PoolAction.Add,
-  PoolAction.Vote,
-];
+const defaultPoolActions = [PoolAction.Unstake, PoolAction.Vote];
 
 /**
  * COMPUTED
