1f5c76949355fb92a84455f7fda81af59b5a6c30
anotherdev <joseluismanco37@gmail.com>
2025-05-27 17:31:53 -0500

    chore: switch balancer for regenerative

--- src/components/contextual/pages/vebal/LMVoting/LMVoting.vue
+++ src/components/contextual/pages/vebal/LMVoting/LMVoting.vue
@@ -171,8 +171,8 @@ watch(account, (_, prevAccount) => {
       </div>
     </div>
 
-    <VotingAlert v-if="veBalLockTooShort" title="veBAL not locked for 7 days">
-      You must have veBAL locked for more than 7 days to vote on gauges.
+    <VotingAlert v-if="veBalLockTooShort" title="veREFI not locked for 7 days">
+      You must have veREFI locked for more than 7 days to vote on gauges.
     </VotingAlert>
 
     <VotingAlert
@@ -180,23 +180,23 @@ watch(account, (_, prevAccount) => {
       title="Resubmit your votes to utilize your full voting power"
     >
       Votes on pools are set at the time of the vote. Since you’ve added new
-      veBAL since your original vote, you have additional voting power which is
+      veREFI since your original vote, you have additional voting power which is
       not being used. Use the 'Edit votes' button to resubmit your votes.
     </VotingAlert>
 
     <VotingAlert
       v-if="noVeBalBalance && !isLoading"
-      title="You need some veBAL to vote on gauges"
+      title="You need some veREFI to vote on gauges"
     >
-      Get veBAL by locking up LP tokens from the 80% BAL / 20% WETH pool.
+      Get veREFI by locking up LP tokens from the 80% REFI / 20% WETH pool.
     </VotingAlert>
 
     <VotingAlert
       v-if="veBalExpired"
-      title="You can't vote because your veBAL has expired"
+      title="You can't vote because your veREFI has expired"
     >
-      You need some veBAL to vote on gauges. Unlock and relock your
-      B-80BAL-20-WETH to get some veBAL.
+      You need some veREFI to vote on gauges. Unlock and relock your
+      B-80BAL-20-WETH to get some veREFI.
     </VotingAlert>
 
     <VotingAlert
