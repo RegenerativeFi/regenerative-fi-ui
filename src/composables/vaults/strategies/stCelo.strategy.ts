fef423b56c0e835909737b9ab8f9ad70734fe860
anotherdev <joseluismanco37@gmail.com>
2025-10-01 19:52:59 -0500

    feat: add vaults rewards to dashboard and improve architecture behind rewards fetching

--- src/composables/vaults/strategies/stCelo.strategy.ts
+++ src/composables/vaults/strategies/stCelo.strategy.ts
@@ -4,7 +4,7 @@ import { MerklApi } from '@merkl/api';
 
 const DEFAULT_DECIMALS = 18;
 const MANAGER_ADDRESS = '0x0239b96D10a434a56CC9E09383077A0490cF9398';
-const VAULT_ADDRESS = '0x1b8c73e2aB2FB34ADA2dFaCD1F59bEAb76B6C410';
+export const VAULT_ADDRESS = '0x1b8c73e2aB2FB34ADA2dFaCD1F59bEAb76B6C410';
 const CELO_ADDRESS = '0x471EcE3750Da237f93B8E339c536989b8978a438';
 
 const ERC20_ABI = [
@@ -104,7 +104,6 @@ async function getApy(): Promise<number> {
     },
   });
   if (status !== 200) throw 'Failed to fetch APY dat';
-  console.debug('APY data:', data);
 
   return data?.reduce((acc, curr) => acc + (curr.apr || 0), 0) || 0;
 }
