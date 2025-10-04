d58ccc7140623484a6248f842a9a131f4a7cbab4
anotherdev <joseluismanco37@gmail.com>
2025-10-04 12:17:11 -0500

    chore: remove stake this to earn extra whem depositing on LP

--- src/components/forms/pool_actions/AddLiquidityForm/components/AddLiquidityPreview/components/Actions.vue
+++ src/components/forms/pool_actions/AddLiquidityForm/components/AddLiquidityPreview/components/Actions.vue
@@ -15,7 +15,6 @@ import { Pool } from '@/services/pool/types';
 import { TransactionActionInfo } from '@/types/transactions';
 import { useJoinPool } from '@/providers/local/join-pool.provider';
 import useNumbers, { FNumFormats } from '@/composables/useNumbers';
-import { usePoolStaking } from '@/providers/local/pool-staking.provider';
 import useWeb3 from '@/services/web3/useWeb3';
 import useNetwork from '@/composables/useNetwork';
 import FeedbackCard from '@/components/cards/FeedbackCard.vue';
@@ -44,7 +43,6 @@ const { t } = useI18n();
 const { fNum } = useNumbers();
 const { addTransaction } = useTransactions();
 const { lockablePoolId } = useVeBal();
-const { isStakablePool } = usePoolStaking();
 const { isMismatchedNetwork } = useWeb3();
 const { poolWeightsLabel } = usePoolHelpers(toRef(props, 'pool'));
 const {
@@ -165,7 +163,7 @@ onUnmounted(() => {
             $t('lockToGetVeBAL')
           }}
         </BalBtn>
-        <BalBtn
+        <!-- <BalBtn
           v-else-if="isStakablePool"
           color="gradient"
           block
@@ -175,7 +173,7 @@ onUnmounted(() => {
           <StarsIcon class="mr-2 h-5 text-orange-300" />{{
             $t('stakeToGetExtra')
           }}
-        </BalBtn>
+        </BalBtn> -->
 
         <BalBtn
           tag="router-link"
