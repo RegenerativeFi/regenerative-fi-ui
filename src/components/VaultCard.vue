456f09a4ee84f77afcf890f05c12593fa63de9ef
anotherdev <joseluismanco37@gmail.com>
2026-01-08 18:16:11 -0500

    chore: unable stCelo vault deposit

--- src/components/VaultCard.vue
+++ src/components/VaultCard.vue
@@ -62,12 +62,56 @@
             @click="openWithdrawModal"
           />
 
-          <BalBtn
-            label="Deposit"
-            color="gradient"
-            class="flex-1 w-full h-12"
-            @click="openDepositModal"
-          />
+          <!-- Deposit Button - Disabled due to CGP218 -->
+          <BalTooltip placement="top" noPad class="flex-1">
+            <template #activator>
+              <BalBtn
+                label="Deposit"
+                color="gray"
+                class="w-full h-12 opacity-50 cursor-not-allowed"
+                disabled
+              />
+            </template>
+            <div
+              class="overflow-hidden w-64 bg-white dark:bg-gray-800 rounded-lg border border-gray-200 dark:border-gray-700 shadow-lg"
+            >
+              <div
+                class="py-3 px-4 border-b border-gray-200 dark:border-gray-700 bg-amber-50 dark:bg-amber-900/20"
+              >
+                <div class="flex gap-2 items-center">
+                  <svg
+                    class="w-4 h-4 text-amber-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                  <span
+                    class="text-sm font-semibold text-amber-700 dark:text-amber-400"
+                  >
+                    Deposits Disabled
+                  </span>
+                </div>
+              </div>
+              <div class="py-3 px-4">
+                <p class="text-sm leading-5 text-gray-600 dark:text-gray-300">
+                  Following
+                  <span class="font-medium text-gray-900 dark:text-white"
+                    >CGP218</span
+                  >, the stCELO Validator strategy no longer yields returns.
+                </p>
+                <p
+                  class="mt-2 text-xs leading-4 text-gray-500 dark:text-gray-400"
+                >
+                  You can still withdraw your stCELO from the vault.
+                </p>
+              </div>
+            </div>
+          </BalTooltip>
         </div>
       </div>
     </div>
@@ -146,9 +190,9 @@ const closeWithdraw = () => {
   showWithdraw.value = false;
 };
 
-const openDepositModal = () => {
-  showDeposit.value = true;
-};
+// const openDepositModal = () => {
+//   showDeposit.value = true;
+// };
 
 const closeDeposit = () => {
   showDeposit.value = false;
